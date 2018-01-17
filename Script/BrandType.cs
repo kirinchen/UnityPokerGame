1c6a5e3265f7feda6a9abad09d61aac64eb3f09d
kirinchen <a29243685@msn.com>
2018-01-17 23:14:38 +0800

    done race result show

---
+++ Script/BrandType.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace surfm.yoar.poker {
+    public enum BrandType {
+        Straight_Flush, Four_of_a_Kind, Full_house, Flush, Straight, Three_of_a_kind, Two_Pairs, One_Pair, High_card
+    }
+}
